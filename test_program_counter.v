6c69f974aa54f1eab814969ea419d0ca9f9d8f10
Ryan Wong <ryanwong935@gmail.com>
2025-01-01 02:04:30 -0500

    Added more test cases.

--- test_program_counter.v
+++ test_program_counter.v
@@ -4,8 +4,8 @@
 module program_counter_test_bench;
    reg clock;
    reg [31:0] pc_in_addr;
-   wire	[31:0] pc_out_addr;
-   
+   wire [31:0] pc_out_addr;
+
    program_counter PC(clock, pc_in_addr, pc_out_addr);
 
    //Generate clock at 50 MHz (20ns).
@@ -20,7 +20,7 @@ module program_counter_test_bench;
       pc_in_addr = 32'h00000000;
 
       //Wait one clock cycle before checking output.
-      #20;
+      #20;      
       if(pc_out_addr != 32'h00000000) begin
 	 $display("Test failed. in = %d, expected = %d", pc_in_addr, pc_out_addr);
 	 $finish;
@@ -33,6 +33,34 @@ module program_counter_test_bench;
 	 $finish;
       end
 
+      pc_in_addr = 32'h00000010;
+      #20;
+      if(pc_out_addr != 32'h00000010) begin
+	 $display("Test failed. in = %d, expected = %d", pc_in_addr, pc_out_addr);
+	 $finish;
+      end
+
+      pc_in_addr = 32'h0000001C;
+      #20;
+      if(pc_out_addr != 32'h0000001C) begin
+	 $display("Test failed. in = %d, expected = %d", pc_in_addr, pc_out_addr);
+	 $finish;
+      end
+
+      pc_in_addr = 32'h12ACE478;
+      #20;
+      if(pc_out_addr != 32'h12ACE478) begin
+	 $display("Test failed. in = %d, expected = %d", pc_in_addr, pc_out_addr);
+	 $finish;
+      end
+
+      pc_in_addr = 32'h03300014;
+      #20;
+      if(pc_out_addr != 32'h03300014) begin
+	 $display("Test failed. in = %d, expected = %d", pc_in_addr, pc_out_addr);
+	 $finish;
+      end
+      
       $display("All tests passed.");
       $finish;
    end
